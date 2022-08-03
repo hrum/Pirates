ee4a55575bf834bc6e0984641393938a23811b60
Dmitry Shuvalov <dmit86@mail.ru>
2022-08-03 15:40:11 +0300

    added Point and Layout

---
+++ app/src/main/java/com/shuvzero/pirates/model/Point.java
@@ -0,0 +1,13 @@
+package com.shuvzero.pirates.model;
+
+public class Point {
+
+    public final float x;
+    public final float y;
+
+    public Point(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
