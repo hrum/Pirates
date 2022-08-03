23827bb1c3bdbdadfd6ca52d2a9623eef29f3d4f
Dmitry Shuvalov <dmit86@mail.ru>
2022-08-03 11:34:40 +0300

    working on treasure map

--- app/src/main/java/com/shuvzero/pirates/view/MainActivity.java
+++ app/src/main/java/com/shuvzero/pirates/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.shuvzero.pirates;
+package com.shuvzero.pirates.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.shuvzero.pirates.R;
 
 public class MainActivity extends AppCompatActivity {
 
