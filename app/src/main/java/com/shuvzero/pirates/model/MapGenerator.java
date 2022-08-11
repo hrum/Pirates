95f2b0679bfd7fa32a9452076053c506401fced9
Dmitry Shuvalov <dmit86@mail.ru>
2022-08-11 16:09:21 +0300

    new pictures

--- app/src/main/java/com/shuvzero/pirates/model/MapGenerator.java
+++ app/src/main/java/com/shuvzero/pirates/model/MapGenerator.java
@@ -119,7 +119,7 @@ public class MapGenerator {
     private void generateSingle() {
         for(Feature feature: Feature.values()) {
             if (feature.isLand() && feature.getFeatureType() == FeatureType.Single) {
-                for (int i = 0; i < 5; i++) {
+                for (int i = 0; i < 7; i++) {
                     int position = emptyLandCells.get(random.nextInt(emptyLandCells.size()));
                     emptyLandCells.remove((Integer) position);
                     map.getCell(position).setFeature(feature);
